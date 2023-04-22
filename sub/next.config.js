a1de8bb44af8952954dd57994f8fc9af5d2b5965
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-22 11:23:21 +0900

    設定変更

--- sub/next.config.js
+++ sub/next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // .ts/.tsxファイルをsrcディレクトリから読み込むための設定
+    config.resolve.alias['@'] = __dirname;
+    return config;
+  },
+  // _app.tsx を含むファイルを src ディレクトリに移動するための設定
+  pages: {
+    // pagesディレクトリ内にある _app.tsx は無視する
+    exclude: [
+      /_app.tsx$/,
+    ],
+    // srcディレクトリから読み込むファイルを指定する
+    files: [
+      'src/pages/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
 }
-
-module.exports = nextConfig
