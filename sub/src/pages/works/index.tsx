bd6f3e4a63d2867e02341c060a143287a81d550d
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-24 22:13:08 +0900

    制作実績 jsonデータをimportしてループ

--- sub/src/pages/works/index.tsx
+++ sub/src/pages/works/index.tsx
@@ -2,6 +2,12 @@ import BackButton from "@/components/backbutton";
 import Bottom from "@/components/bottom";
 import Header from "@/components/header";
 import UnderPageTitle from "@/components/underPageTitle";
+import worksListData from "@/data/worksList.json";
+
+type WorksList = {
+  url: string;
+  title: string;
+}
 
 export default function Work() {
   return (
@@ -18,45 +24,11 @@ export default function Work() {
           </p>
         <div className="flex justify-center">
           <ul className="text-color02 text-lg text-type02">
-            <li className="mb-4">
-              <a href="" target="_blank" className="duration-500 hover:text-color03">R.YUKI Portfolio</a>
+            {worksListData.map((worksList: WorksList) => (
+            <li key={worksList.url} className="mb-4">
+              <a href={worksList.url} target="_blank" className="duration-500 hover:text-color03">{worksList.title}</a>
             </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/pokeccha/" target="_blank" className="duration-500 hover:text-color03">pokeccha</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/horoyoi-meter/" target="_blank" className="duration-500 hover:text-color03">ほろよいメーター</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/keyaki-nurse/index.html" target="_blank" className="duration-500 hover:text-color03">けやき坂中央病院 看護師採用サイト</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/diversity/" target="_blank" className="duration-500 hover:text-color03">和歌山のダイビングショップ Diversity</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/yae-zakura-karate/" target="_blank" className="duration-500 hover:text-color03">神戸・大阪・京都の空手道場 八重櫻</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/portfolio/hinata-hair.html" target="_blank" className="duration-500 hover:text-color03">hair salon hinata</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/hug-me/" target="_blank" className="duration-500 hover:text-color03">ペット専用のフォトスタジオ STUDIO hug-me</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/bootstrap/uzbekistan.html" target="_blank" className="duration-500 hover:text-color03">Uzbekistan Trip by Bootstrap</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/portfolio/hina-mobile.html" target="_blank" className="duration-500 hover:text-color03">格安スマホ hina mobile</a>
-            </li>
-            <li className="mb-4">
-              <a href="https://www.yuki-017.com/portfolio/nogi-suit-newyear.html" target="_blank" className="duration-500 hover:text-color03">メンズスーツ Nogi-Suit 特集ページ</a>
-            </li>
-            <li className="mb-4">
-              <a href="" target="_blank" className="duration-500 hover:text-color03"></a>
-            </li>
-            <li className="mb-4">
-              <a href="" target="_blank" className="duration-500 hover:text-color03"></a>
-            </li>
+            ))}
           </ul>
         </div>
       </div>
