0cbdbb8aa5da2c0a79f82b5d0a55a3eaabcee77d
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2025-06-05 21:53:47 +0900

    GA4タグ挿入

--- sub/src/pages/_app.tsx
+++ sub/src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import '../styles/globals.css'
+import Script from 'next/script';
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Component {...pageProps} />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-SWF8LG2FZN`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-SWF8LG2FZN', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Component {...pageProps} />
     </>
   )
 }
--- sub/src/pages/_document.tsx
+++ sub/src/pages/_document.tsx
@@ -1,10 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="ja">
 
       <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-SWF8LG2FZN`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SWF8LG2FZN', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         <title>ゆきのせかい。</title>
         <meta name="description" content="ゆきのせかい。 | 作品のひろば | デザイン・コーディング・Webアプリ" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
